d90f8be49956c5cefbf6fb5646b9291ba5f9ed88
chongchen <chongchen1999@gmail.com>
2023-12-11 15:42:57 -0500

    add banner swiper

--- src/components/UI/home/banner/BannerSwiper.jsx
+++ src/components/UI/home/banner/BannerSwiper.jsx
@@ -1,6 +1,23 @@
-import {Link} from "react-router-dom";
+import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import Swiper from 'swiper/bundle';
+
 
 const BannerSwiper = () => {
+    useEffect(() => {
+        new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-ke-next',
+                prevEl: '.swiper-ke-prev',
+            },
+        });
+    }, []);
+
     return (
         <div className="swiper-container swiper-ke-banner">
             <div className="swiper-wrapper">
@@ -9,11 +26,9 @@ const BannerSwiper = () => {
                 <Link to="/Iberian" target="_blank" className="swiper-slide" style={{ backgroundImage: 'url(src/assets/images/iberian.webp)' }}></Link>
             </div>
 
-
             <div className="swiper-ke-next"><i className="iconfont icon-nex"></i></div>
             <div className="swiper-ke-prev"><i className="iconfont icon-prev"></i></div>
 
-            {/* 自定义分页器 */}
             <div className="swiper-pagination" style={{ bottom: '70px', width: '1200px', left: '50%' }}></div>
         </div>
     );
